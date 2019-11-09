088ad8dd30f24e122e1b220c4aa5fa4103f13a05
DarkAce65 <tvdood99@gmail.com>
2019-11-09 01:03:57 -0500

    Add flag to render to window

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+extern crate clap;
 extern crate image;
+extern crate minifb;
 
+use clap::{App, Arg};
 use image::{Pixel, RgbaImage};
+use minifb::{Key, Window, WindowOptions};
+use std::convert::TryInto;
 
 const WIDTH: u32 = 256;
 const HEIGHT: u32 = 256;
 
 fn main() {
+    let matches = App::new("raytracer")
+        .arg(
+            Arg::with_name("framebuffer")
+                .short("f")
+                .long("framebuffer")
+                .help("Runs the raytracer in a window"),
+        )
+        .get_matches();
+
+    let use_framebuffer = matches.is_present("framebuffer");
+
     let mut image = RgbaImage::new(WIDTH, HEIGHT);
     for (x, y, color) in image.enumerate_pixels_mut() {
         let channels = color.channels_mut();
@@ -14,5 +30,27 @@ fn main() {
         channels[3] = 255;
     }
 
-    image.save("output.png").expect("Unable to write image");
+    if use_framebuffer {
+        let w = WIDTH as usize;
+        let h = HEIGHT as usize;
+
+        let mut buffer: Vec<u32> = Vec::new();
+        for pixel in image.into_vec().chunks_exact(4) {
+            buffer.push(u32::from_le_bytes(pixel.try_into().unwrap()));
+        }
+
+        let mut window: Window = Window::new("raytracer", w, h, WindowOptions::default())
+            .unwrap_or_else(|e| {
+                panic!("{}", e);
+            });
+
+        println!("Rendering to window. Press escape to exit");
+        while window.is_open() && !window.is_key_down(Key::Escape) {
+            window.update_with_buffer(&buffer).unwrap();
+        }
+    } else {
+        let filename = "output.png";
+        image.save(filename).expect("Unable to write image");
+        println!("Output written to {}", filename);
+    }
 }
