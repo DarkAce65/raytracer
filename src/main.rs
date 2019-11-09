1ead1750a0f81bb0f12c88bfc06e1a45f6e84ec0
DarkAce65 <tvdood99@gmail.com>
2019-11-09 00:03:11 -0500

    Write simple color test image generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate image;
+
+use image::{Pixel, RgbaImage};
+
+const WIDTH: u32 = 256;
+const HEIGHT: u32 = 256;
+
 fn main() {
-    println!("Hello, world!");
+    let mut image = RgbaImage::new(WIDTH, HEIGHT);
+    for (x, y, color) in image.enumerate_pixels_mut() {
+        let channels = color.channels_mut();
+        channels[0] = (255 * x / WIDTH) as u8;
+        channels[1] = (255 * y / WIDTH) as u8;
+        channels[3] = 255;
+    }
+
+    image.save("output.png").expect("Unable to write image");
 }
